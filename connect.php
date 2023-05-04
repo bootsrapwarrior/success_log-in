3bd14f97179538e0e7505ee993542cdaa9e6e5bb
bootsrapwarrior <129391680+bootsrapwarrior@users.noreply.github.com>
2023-05-04 19:50:37 +0800

    Add files via upload

---
+++ connect.php
@@ -0,0 +1,18 @@
+<?php
+require_once 'includes/header.php';
+?>
+  <?php
+$servername = "localhost";
+$username = "root";
+$password = "";
+
+// Create connection
+$conn = new mysqli($servername, $username, $password);
+
+// Check connection
+if ($conn->connect_error) {
+  die("Connection failed: " . $conn->connect_error);
+}
+echo "Connected successfully!";
+?>
+<?php require_once 'includes/footer.php'?>
